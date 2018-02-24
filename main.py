b941ea173dd764322e65475bbd05aa321972b02f
Matija Rifl <matija.rifl@gmail.com>
2018-02-24 17:31:36 +0100

    prva-verzija

--- main.py
+++ main.py
@@ -26,11 +26,82 @@ class BaseHandler(webapp2.RequestHandler):
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
 
+class DNKAnaliza:
+    def __init__(self, DNKstring):
+        self.DNKstring = DNKstring
+
+    def barva_las(self):
+        if "CCAGCAATCGC" in self.DNKstring:
+            return "crna"
+        elif "GCCAGTGCCG" in self.DNKstring:
+            return "rjava"
+        elif "TTAGCTATCGC" in self.DNKstring:
+            return "korencek"
+        else:
+            return "nedefinirana"
+
+    def oblika_obraza(self):
+        if "GCCACGG" in self.DNKstring:
+            return "kvadraten"
+        elif "ACCACAA" in self.DNKstring:
+            return "okrogel"
+        elif "AGGCCTCA" in self.DNKstring:
+            return "ovalen"
+        else:
+            return "nedefiniran"
+
+    def barva_oci(self):
+        if "TTGTGGTGGC" in self.DNKstring:
+            return "modra"
+        elif "GGGAGGTGGC" in self.DNKstring:
+            return "zelena"
+        elif "AAGTAGTGAC" in self.DNKstring:
+            return "rjava"
+        else:
+            return "nedefiniran"
+
+    def spol(self):
+        if "TGCAGGAACTTC" in self.DNKstring:
+            return "moski"
+        elif "TGAAGGACCTTC" in self.DNKstring:
+            return "zenska"
+        else:
+            return "nedefiniran"
+
+    def rasa(self):
+        if "AAAACCTCA" in self.DNKstring:
+            return "belec"
+        elif "CGACTACAG" in self.DNKstring:
+            return "crnec"
+        elif "CGCGGGCCG" in self.DNKstring:
+            return "azijec"
+        else:
+            return "nedefeniran"
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("hello.html")
+        return self.render_template("main.html")
+
+class ResultHandler(BaseHandler):
+    def post(self):
+
+        DNK = DNKAnaliza(self.request.get("DNKosebe"))
+        barvalas = DNK.barva_las()
+        oblikaobraza = DNK.oblika_obraza()
+        barvaoci = DNK.barva_oci()
+        spoll = DNK.spol()
+        rasaa = DNK.rasa()
+
+        podatki = {"alasje": barvalas,
+                   "aobraz": oblikaobraza,
+                   "aoci": barvaoci,
+                   "aspol": spoll,
+                   "arasa": rasaa}
+
+        return self.render_template("result.html", podatki)
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
+    webapp2.Route('/result', ResultHandler)
+
 ], debug=True)
